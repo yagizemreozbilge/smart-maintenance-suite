860efdfb45b3e3e7646f908ee66d9b0004e256f3
yagizemreozbilge <yagizozbilge@gmail.com>
2026-02-04 23:39:38 +0300

    building frontend installing packages

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 1. Ana esbuild derleyicisine .js dosyalarını 'jsx' olarak oku diyoruz
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  // 2. Başlangıçtaki bağımlılık tarayıcısına (optimizasyon) aynı talimatı veriyoruz
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
